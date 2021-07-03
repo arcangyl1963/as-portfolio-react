0471a4b0516ef5cea87964ea004fbdde70ed7b55
Jim Harris <arcangyl@gmail.com>
2021-07-03 14:34:20 -0400

    add the empty script files for footer, header,p portfolio, and resume to Components

---
+++ src/Components/header.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        if(this.props.data) {
+            var name = this.props.data.name;
+            var org = this.props.data.organization
+            var jobtitle = this.props.data.jobtitle;
+            var desc = this.props.data.desc;
+            var networks = this.props.social.map(function(network) {
+                return <li key={network.name}><a href={network.url} target="_blank" rel="noreferrer"><i className="{network.className}"></i></a></li>
+            })
+        }
+        return (
+            <header id="home">
+                <nav id="nav-wrap">
+                    <a className="mobile-btn" href="#nav=wrap" title="Show Navigation">Show Navigation</a>
+                    <a className="mobile-btn" href="#home" title="Hide Navigation">Hide Navigation</a>
+                    <ul id="nav" className="nav">
+                        <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+                        <li><a className="smoothscroll" href="#portfolio">Portfolio</a></li>
+                        <li><a className="smoothscroll" href="#contact">Contact</a></li>
+                        <li><a className="smoothscroll" href="#resume">Resume</a></li>
+                        <li><a className="smoothscroll" href="#linkedin"><i className="fab fa-linkedin"></i>LinkedIn</a></li>
+                    </ul>
+                </nav>
+                <div className="row banner">
+                    <div>
+                        <h1 className="responsive-headline">{org}</h1>
+                        <h2 className="responsive-headline">{name}</h2>
+                        <h3>A <span>{jobtitle}</span> from Atlanta, GA. {desc}</h3>
+                        <hr />
+                        <ul className="social">
+                            {networks}
+                        </ul>
+
+                    </div>
+                </div>
+                <p className="scrolldown">
+                    <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
+                </p>
+            </header>
+        );
+    }
+}
+export default Header;
