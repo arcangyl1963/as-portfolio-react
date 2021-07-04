a586a33c4fd58c3c2cabe4cd426dc647d3bef81c
Jim Harris <arcangyl@gmail.com>
2021-07-04 16:13:15 -0400

    added code for the App script

--- src/App.js
+++ src/App.js
@@ -3,31 +3,47 @@ import $ from 'jquery';
 import './App.css';
 import Header from './Components/header';
 import About from './Components/about';
+import Footer from './Components/footer';
+import Resume from './Components/resume';
+import Contact from './Components/contact';
+import Portfolio from './Components/portfolio';
 
 class App extends Component {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            foo: 'bar',
+            resumeData: {}
+        };
+    }
+    fetchResumeData() {
+        $.ajax({
+            url: '/resumeData.json',
+            dataType: 'json',
+            cache: false,
+            success: function(data) {
+                this.setState({resumeData: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.log(err);
+                alert(err);
+            }
+        });
+    }
+    componentDidMount() {
+        this.fetchResumeData();
+    }
+    render() {
+        return (
+            <div className="App">
+                <Header data = {this.state.resumeData.main}/>
+                <About data = {this.state.resumeData.main}/>
+                <Resume data = {this.state.resumeData.resume}/>
+                <Portfolio data = {this.state.resumeData.portfolio}/>
+                <Contact data = {this.state.resumeData.main}/>
+                <Footer data = {this.state.resumeData.main}/>
+            </div>
+        );
+    }
 }
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
 export default App;
