5fa2e5775adf5bcc3ea4a428ac11f6f49ab236e6
Jim Harris <arcangyl@gmail.com>
2021-07-04 15:45:02 -0400

    Added code for footer, portfolio, and resume to Components

---
+++ src/Components/footer.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+        if (this.props.data) {
+            var networks = this.props.data.social.map( function(network) {
+                return <li key={network.name}><a href={network.url} target="_blank" rel="noreferrer"><i className={network.className}></i></a></li>
+            })
+        }
+        return (
+            <footer>
+                <div className="row">
+                    <div className="col-12 columns">
+                        <ul className="social-links">
+                            {networks}
+                        </ul>
+                        <ul className="copyright">
+                            <li>&copy; Copyright 2021 Arcangy Studios</li>
+                        </ul>
+                    </div>
+                    <div id="to-top">
+                        <a className="smoothsroll" title="Return to Top" href="#home"><i className="icon-up-open"></i></a>
+                    </div>
+                </div>
+            </footer>
+        );
+    }
+}
+export default Footer;
---
+++ src/Components/portfolio.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class Portfolio extends Component {
+    render() {
+        if (this.props.data) {
+            var projects = this.data.projects.map(function(projects) {
+                var projImg = 'images/portfolio/' = projects.image;
+            return
+            <div key={projects.title} className="columns portfolio-item">
+                <div className="item-wrap">
+                    <a href={projects.url} title={projects.title} target="_blank" rel="noreferrer">
+                        <img alt={projects.title} src={projImg} />
+                        <div className="overlay">
+                            <div className="portfolio-item-meta">
+                                <h5>{projects.title}</h5>
+                                <p>{projects.category}</p>
+                            </div>
+                        </div>
+                        <div className="link-icon"><i className="fa fa-link"></i>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            })
+        }
+        return (
+            <section id="portfolio">
+                <div className="row">
+                    <div className="col-12 columns collapsed">
+                        <h1>Please review some of my work:</h1>
+                        <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+                            {projects}
+                        </div>
+                    </div>
+                </div>
+            </section>
+        );
+    }
+}
+export default Portfolio;
