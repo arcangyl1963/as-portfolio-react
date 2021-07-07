aac733e66355058b35eb8955b8838e9331ec10ea
Jim Harris <arcangyl@gmail.com>
2021-07-07 00:13:26 -0400

    Fixed the element structure in about.js that was causing some layout issues

--- src/Components/about.js
+++ src/Components/about.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React, { Component } from "react";
 
 class About extends Component {
@@ -8,21 +9,30 @@ class About extends Component {
             var downloadResume = this.props.data.downloadresume;
         }
         return (
-            <section id="about">
+          <section id="about">
             <div className="row">
-                <div className="three columns"></div>
+              <div className="three columns">
                 <img className="profile-img" src={profImg} alt="James Harris" />
-            </div>
-            <div className="nine columns main-col">
+              </div>
+              <div className="nine columns main-col">
                 <h2>About Arcangyl Studios</h2>
                 <p>{bio}</p>
-                <div className="column download">
+                <div className="row">
+                  <div className="columns download">
                     <p>
-                        <a href={downloadResume} target="_blank" rel="noreferrer" className="button"><i className="fa fa-download"></i>Download Resume</a>
+                      <a
+                        href={downloadResume}
+                        target="_blank"
+                        className="button"
+                      >
+                        <i className="fa fa-download"></i>Download Resume
+                      </a>
                     </p>
+                  </div>
                 </div>
+              </div>
             </div>
-            </section>
+          </section>
         );
     }
 }
